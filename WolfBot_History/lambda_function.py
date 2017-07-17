26f6558c0095a81ba74c77525b8db0987414d4d3
gpaw789 <georgepaw789@hotmail.com>
2017-07-17 23:49:31 +1000

    First Upload

---
+++ WolfBot_History/lambda_function.py
@@ -0,0 +1,351 @@
+import math
+import sys
+import dateutil.parser
+import datetime
+import time
+import os
+import logging
+import csv
+#import urllib.request
+from yahoo_finance import Share
+#experimental
+import matplotlib
+matplotlib.use('agg',warn=False, force=True)
+from matplotlib import pyplot as plt
+from imgurpython import ImgurClient
+
+
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+""" --- Helpers to build responses which match the structure of the necessary dialog actions --- """
+def get_slots(intent_request):
+    return intent_request['currentIntent']['slots']
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    return {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'ElicitSlot',
+            'intentName': intent_name,
+            'slots': slots,
+            'slotToElicit': slot_to_elicit,
+            'message': message
+        }
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+
+""" --- Helper Functions --- """
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    if message_content is None:
+        return {
+            "isValid": is_valid,
+            "violatedSlot": violated_slot,
+        }
+
+    return {
+        'isValid': is_valid,
+        'violatedSlot': violated_slot,
+        'message': {'contentType': 'PlainText', 'content': message_content}
+    }
+
+def isvalid_date(date):
+    try:
+        dateutil.parser.parse(date)
+        return True
+    except ValueError:
+        return False
+
+def lookup2(symbol):
+
+    try:
+        share = Share(symbol)
+    except:
+        return None
+
+
+    if share.get_name() is None:
+        return None
+
+    return {
+        "name": share.get_name(),
+        "symbol": symbol.upper(),
+        "price": share.get_price(),
+        "change": share.get_change(),
+        "pc_change": share.get_percent_change(),
+        "trade_time": share.get_trade_datetime()
+
+    }
+
+
+def validate_action_WolfBot_History(symbol, start_date, end_date):
+    #validate the action of WolfBot_History, if there are missing slots then elicit them
+
+    if symbol == (None or ""):
+        return build_validation_result(False, 'symbol', 'Please state a symbol. E.g. $AMZN')
+
+    if (start_date is None) or (not isvalid_date(start_date)):
+        return build_validation_result(False, 'start_date', 'Please state start date. E.g. {}'.format(datetime.date.today() - datetime.timedelta(days=30)))   #display a start date 30 days prior
+    elif datetime.datetime.strptime(start_date, '%Y-%m-%d').date() >= datetime.date.today():        #can't go into the future
+        return build_validation_result(False, 'start_date', 'Please pick a date in the past. E.g. 16/06/2017')
+
+    if (end_date is None) or not (isvalid_date(end_date)):
+        return build_validation_result(False, 'end_date','Please state end date. E.g. {}'.format(datetime.date.today()))
+    elif (datetime.datetime.strptime(end_date, '%Y-%m-%d').date() > datetime.date.today()) \
+            or (datetime.datetime.strptime(end_date, '%Y-%m-%d').date() < datetime.datetime.strptime(start_date,'%Y-%m-%d').date()):  # can't go into the future and before the start date
+        return build_validation_result(False, 'end_date', 'Please pick a date after the start date. E.g. 16/06/2017')
+
+    return build_validation_result(True, None, None)
+
+
+
+""" --- Functions that control the bot's behavior --- """
+
+
+def action_WolfBot_History(intent_request):
+    """
+    Performs dialog management and fulfillment for doing stock history
+    Beyond fulfillment, the implementation of this intent demonstrates the use of the elicitSlot dialog action
+    in slot validation and re-prompting.
+    """
+    symbol = get_slots(intent_request)['symbol']
+
+    #get symbol data
+    if symbol is None:
+        try:
+            utter = intent_request["inputTranscript"]
+
+        except:
+            utter = "This is empty"
+
+        #extract symbol from utter
+        try:
+            #break the string into a list, and then pick the one with the $ symbol
+            phrase_list = utter.split(" ")
+            for element in phrase_list:
+                if "$" in element:
+                    symbol_manual = element.strip("$")
+                    get_slots(intent_request)['symbol'] = symbol_manual     #force the value in, this allows to break out of the strict slot criteria
+                    symbol = symbol_manual
+
+                else:
+                    symbol = ""
+        except:                  #if there's no element, aka no check $
+            return close(intent_request['sessionAttributes'],
+                         'Fulfilled',
+                         {'contentType': 'PlainText',
+                          'content': 'Closing1... Try again with a valid symbol. E.g. $AMZN'})
+
+    # prepping the values
+    slots = get_slots(intent_request)
+    symbol = get_slots(intent_request)['symbol']
+
+    #using last days method, force values into intent_request
+    try:
+        if get_slots(intent_request)['days'] is not None:
+            get_slots(intent_request)['start_date'] = str(datetime.date.today() - datetime.timedelta(days=int(get_slots(intent_request)['days'])))
+            get_slots(intent_request)['end_date'] = str(datetime.date.today())
+    except KeyError:
+        pass
+
+    start_date = get_slots(intent_request)['start_date']
+    end_date = get_slots(intent_request)['end_date']
+
+    source = intent_request['invocationSource']
+
+    # verify symbol
+    try:
+        #scrub out the dollar symbol
+        if '$' in symbol:
+            symbol = symbol.strip('$')
+        symbol_result = Share(symbol)
+        if symbol_result.get_price() is None:
+            return close(intent_request['sessionAttributes'],
+                         'Fulfilled',
+                         {'contentType': 'PlainText',
+                          'content': 'Closing2... {}'.format(symbol)})
+    except:
+        return close(intent_request['sessionAttributes'],
+                     'Fulfilled',
+                     {'contentType': 'PlainText',
+                      'content': 'Closing3... {} {} {} {}'.format(symbol, start_date, end_date, sys.exc_info()[0])})
+
+
+
+
+    #start sussing it out
+    if source == "DialogCodeHook":
+        #determine the symbol
+        symbol = get_slots(intent_request)['symbol']
+        # Using date questioning method (default)
+        # Perform basic validation on the supplied input slots.
+        # Use the elicitSlot dialog action to re-prompt for the first violation detected.
+        validation_result = validate_action_WolfBot_History(symbol, start_date, end_date)
+        if not validation_result['isValid']:
+            slots[validation_result['violatedSlot']] = None
+            return elicit_slot(intent_request['sessionAttributes'],
+                               intent_request['currentIntent']['name'],
+                               slots,
+                               validation_result['violatedSlot'],
+                               validation_result['message'])
+
+    #scrub out the dollar symbol - why is this everywhere? because I don't know what keeps adding the $ back into symbol lol
+
+    if '$' in symbol:
+        symbol = symbol.strip('$')
+
+    symbol_result_history = Share(symbol)
+    try:
+        symbol_result = lookup2(symbol)
+        symbol_history = symbol_result_history.get_historical2(start_date, end_date)
+    except:
+        return close(intent_request['sessionAttributes'],
+                     'Fulfilled',
+                     {'contentType': 'PlainText',
+                      'content': 'Closing4... Try again with a valid symbol. E.g. $AMZN'})
+
+    #formatting
+    trade_time = symbol_result["trade_time"].split(" UTC"); trade_time = trade_time[0]
+
+    #building the content to deliver
+    content = ""
+    #build with first day and last day
+    content = "{} starts at {:.2f} on {} and ends at {:.2f} on {}".format(
+        symbol, float(symbol_history[0]["Adj_Close"]), symbol_history[0]["Date"],
+        float(symbol_history[-1]["Adj_Close"]), symbol_history[-1]["Date"]
+    )
+
+
+    #staging imgur client
+    client_id = 'e8c4821cd07428e'
+    client_secret = 'd943f1ec060983f8bb41ffcfb8efc2e44f569ca6'
+    client = ImgurClient(client_id, client_secret)
+
+    # filter the output
+    output_filter = []
+    for element_dict in symbol_history:
+        output_filter.append({k: element_dict[k] for k in element_dict.keys() & {'Date', 'Adj_Close'}})
+
+    # set up X data
+    X_date = []
+    for element_dict in output_filter:
+        X_date.append(element_dict['Date'])
+
+    # set up Y data
+    Y_close = []
+    for element_dict in output_filter:
+        Y_close.append(element_dict['Adj_Close'])
+
+    # use xticks because matplotlib doesn't like strings
+    plt.plot(Y_close)
+    plt.xticks(range(len(X_date)), X_date)
+    plt.title("{} Starts: {} Ends: {}".format(symbol, start_date, end_date))
+
+    #make sure its empty
+    try:
+        os.remove('/tmp/foo.png')
+    except:
+        pass
+    #save, generate, delete
+    plt.savefig('/tmp/foo.png')
+    callback = client.upload_from_path("/tmp/foo.png", config=None, anon=True)
+    imgur_link = callback['link']
+    plt.gcf().clear()
+    os.remove('/tmp/foo.png')
+
+    '''
+    return close(intent_request['sessionAttributes'],
+                 'Fulfilled',
+                 {'contentType': 'PlainText',
+                  'content': content + "  " + imgur_link})
+    '''
+    #using response card
+    response = {
+        "sessionAttributes": intent_request["sessionAttributes"],
+        "dialogAction": {
+            "type": "Close",
+            'fulfillmentState': "Fulfilled",
+            "message": {
+                "content": "Showing ${} between {} and {}".format(symbol_result["symbol"], start_date, end_date),
+                "contentType": "PlainText"
+            },
+            "responseCard": {
+                "version": 1,
+                "contentType": "application/vnd.amazonaws.card.generic",
+                "genericAttachments": [
+                    {
+                        "title": "{}".format(symbol_result["name"]),
+                        "subTitle": "{} {} ({}) {}".format(
+                            symbol_result["price"],symbol_result["change"],symbol_result["pc_change"], trade_time),
+                        "imageUrl": "{}".format(imgur_link),
+                        "buttons":[
+                            {
+                                "text": "Buy",
+                                "value": "buy"
+                            },
+                            {
+                                "text": "Sell",
+                                "value": "sell"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+
+    return response
+
+""" --- Intents --- """
+
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['currentIntent']['name']
+
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'WolfBot_HistoryC':
+        return action_WolfBot_History(intent_request)
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
+
+
+""" --- Main handler --- """
+
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+    # By default, treat the user request as coming from the America/New_York time zone.
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    logger.debug('event.bot.name={}'.format(event['bot']['name']))
+
+    return dispatch(event)
---
+++ WolfBot_History/yahoo_finance/gp.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+"""
+get-yahoo-quotes.py:  Script to download Yahoo historical quotes using the new cookie authenticated site.
+ Usage: get-yahoo-quotes SYMBOL
+ History
+ 06-03-2017 : Created script
+"""
+
+__author__ = "Brad Luicas & George Paw"
+__copyright__ = "Copyright 2017, Brad Lucas"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Brad Lucas"
+__email__ = "brad@beaconhill.com"
+__status__ = "Production"
+
+
+import re
+import sys
+import time
+import datetime
+import requests
+
+
+def split_crumb_store(v):
+    return v.split(':')[2].strip('"')
+
+
+def find_crumb_store(lines):
+    # Looking for
+    # ,"CrumbStore":{"crumb":"9q.A4D1c.b9
+    for l in lines:
+        if re.findall(r'CrumbStore', l):
+            return l
+    print ("Did not find CrumbStore")
+
+
+def get_cookie_value(r):
+    return {'B': r.cookies['B']}
+
+
+def get_page_data(symbol):
+    url = "https://finance.yahoo.com/quote/%s/?p=%s" % (symbol, symbol)
+    r = requests.get(url)
+    cookie = get_cookie_value(r)
+    #lines = r.text.encode('utf-8').strip().replace('}', '\n')
+    lines = str(r.content).strip().replace('}', '\n')
+    return cookie, lines.split('\n')
+
+
+def get_cookie_crumb(symbol):
+    cookie, lines = get_page_data(symbol)
+    crumb = split_crumb_store(find_crumb_store(lines))
+    # Note: possible \u002F value
+    # ,"CrumbStore":{"crumb":"FWP\u002F5EFll3U"
+    # FWP\u002F5EFll3U
+    #crumb2 = crumb.decode('unicode-escape')
+    crumb2 = crumb
+    return cookie, crumb2
+
+
+def get_data(symbol, start_date, end_date, cookie, crumb):
+    print (cookie)
+    print (crumb)
+
+    filename = '%s.csv' % (symbol)
+    url = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=history&crumb=%s" % (symbol, start_date, end_date, crumb)
+    response = requests.get(url, cookies=cookie)
+    with open (filename, 'w') as handle:
+        for block in response.iter_content(1024):
+            handle.write(str(block))
+
+def get_data2(symbol, start_date, end_date, cookie, crumb):
+
+    url = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=history&crumb=%s" % (symbol, start_date, end_date, crumb)
+    response = requests.get(url, cookies=cookie)
+    data_string = response.text
+    return data_string
+
+
+def get_now_epoch():
+    # @see https://www.linuxquestions.org/questions/programming-9/python-datetime-to-epoch-4175520007/#post5244109
+    return int(time.mktime(datetime.datetime.now().timetuple()))
+
+
+def download_quotes(symbol):
+    start_date = 0
+    end_date = get_now_epoch()
+    cookie, crumb = get_cookie_crumb(symbol)
+    get_data(symbol, start_date, end_date, cookie, crumb)
+
+
+def get_hist(symbol, start_date, end_date):
+    hist = []; retry = 2
+    start_date = int(time.mktime(datetime.datetime.strptime(start_date, '%Y-%m-%d').date().timetuple()))
+    end_date_real = datetime.datetime.strptime(end_date, '%Y-%m-%d').date() + datetime.timedelta(days=1)    #for some reason the yahoo api is exlcuding the end date, need to pad it with one more day
+    end_date = int(time.mktime(end_date_real.timetuple()))
+    cookie, crumb = get_cookie_crumb(symbol)
+    data_string = get_data2(symbol, start_date, end_date, cookie, crumb)
+    while ("cookie" in data_string) and retry > 0:     #run it one more time
+        print ("Retrying to get cookie.... {} retries left".format(retry))
+        retry = retry - 1
+        cookie, crumb = get_cookie_crumb(symbol)
+        data_string = get_data2(symbol, start_date, end_date, cookie, crumb)
+    data_list = data_string.split('\n')
+    keys_string = data_list.pop(0); keys_string = keys_string.replace("Adj Close", "Adj_Close", 1);keys = keys_string.split(',')
+    for element in data_list:
+        if element is not '':
+            zipped = dict(zip(keys,element.split(',')))
+            zipped['Symbol'] = symbol       #make it more like yahoo-finance output
+            hist.append(zipped)
+    return hist
+
+
+if __name__ == '__main__':
+    # If we have at least one parameter go ahead and loop overa all the parameters assuming they are symbols
+    if len(sys.argv) == 1:
+        print ("\nUsage: get-yahoo-quotes.py SYMBOL\n\n")
+    else:
+        for i in range(1, len(sys.argv)):
+            symbol = sys.argv[i]
+            print ("--------------------------------------------------")
+            print ("Downloading %s to %s.csv" % (symbol, symbol))
+            download_quotes(symbol)
+            print ("--------------------------------------------------")
+
